80c18f407ab18ecb69ae525b82c585d49692d9f4
Pritesh Hirani <pritesh.hirani@klarna.com>
2023-12-13 05:22:41 +0100

    fix: export lineitem type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { compareWithLabeled } from './compareWithLabeled'
 export { MatchKey, APS_FIELDS } from './constants'
-export type { ComparisonInput, ComparisonResult } from './types'
+export type { ComparisonInput, ComparisonResult, LineItemWithMultiPossibleValues } from './types'
